86bb4adcfdaf88e2cafd11b7012adc257e0e91bb
SashoJoto <sasho2468@gmail.com>
2024-12-27 19:32:10 +0200

    wip

---
+++ src/main/java/com/sashojoto/harvesthub/HarvestHubApplication.java
@@ -0,0 +1,13 @@
+package com.sashojoto.harvesthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarvestHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sashojoto/harvesthub/HarvestHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashojoto.harvesthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
